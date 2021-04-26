71c13741726d607e6d39a30abdfca3f8b6d48844
Brandon Redmond <brandon125@Brandons-MacBook-Pro.local>
2021-04-26 18:38:49 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+/*
+FUNCTIONS EXERCISE 1
+Skills: Functions
+● Write a function named getAreaOfCircle. It takes a radius parameter. It calculates and
+returns the area of a circle with that radius.
+● Write a function named getCircumferenceOfCircle. It takes a radius parameter. It
+calculates and returns the circumference of a circle with that radius.
+● Write a function named getAreaOfSquare. It takes a side parameter. It calculates and
+returns the area of a square with that side length.
+● Write a function named getAreaOfTriangle. It takes two parameters: base and height.
+It calculates and returns the area of a triangle with that base and height.
+Call each of these functions and log the result to the console. (NOTE: None of these functions
+should use console.log within them. Instead, they must return the calculated value.)
+*/
+
+    function getAreaOfCircle () {
+        area = null;
+        return area
+    } 
+
+    function getCircumferenceOfCircle() {
+        circumference = null;
+        return circumference;
+    }
+
+    function getAreaOfSquare () {
+        area = null;
+        return area;
+    }
+    function getAreaOfTriangle () {
+        area = null;
+        return area;
+    }
+console.log(`circle area is ${getAreaOfCircle()}, circle circumference is ${getCircumferenceOfCircle()}, square area is ${getAreaOfSquare()}, triangle area is ${getAreaOfTriangle()}`)
